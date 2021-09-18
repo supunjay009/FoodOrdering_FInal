1edcd0f84f82d9d05fa0fac2781e1af646d760a8
Adeesha Peiris <wap.rulz@gmail.com>
2021-09-18 20:08:06 +0530

    Added kitchen activity

---
+++ app/src/main/java/com/example/foodordering/Activity/kitchenActivity.java
@@ -0,0 +1,16 @@
+package com.example.foodordering.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.foodordering.R;
+
+public class kitchenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kitchen);
+    }
+}
