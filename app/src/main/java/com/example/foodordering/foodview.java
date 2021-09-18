1761871774a6e6b10f317e53e59b073388153986
Ashen Thiwanka <ashenthiwanka1999@outlook.com>
2021-09-19 00:18:45 +0530

    ashen food view complete ui

--- app/src/main/java/com/example/foodordering/foodview.java
+++ app/src/main/java/com/example/foodordering/foodview.java
@@ -1,14 +1,29 @@
 package com.example.foodordering;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 import android.os.Bundle;
 
 public class foodview extends AppCompatActivity {
-
+    int minteger = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foodview);
+    }
+    public void increaseInteger(View view) {
+        minteger = minteger + 1;
+        display(minteger);
+
+    }public void decreaseInteger(View view) {
+        minteger = minteger - 1;
+        display(minteger);
+    }
+
+    private void display(int number) {
+        TextView displayInteger = (TextView) findViewById(R.id.qty);
+        displayInteger.setText("" + number);
     }
 }
