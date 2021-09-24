9ad732aac8447dfef5ad78c348b19eca25d7c56a
Adeesha Peiris <wap.rulz@gmail.com>
2021-09-25 00:32:13 +0530

    DB integration complete - 1.1

---
+++ app/src/main/java/com/example/foodordering/Activity/Activity/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.example.foodordering.Activity.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import com.example.foodordering.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_splash);
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+    }
+}
